d03b4c0de20d11090aeb1ac320364df79a4badb0
jschoi <jschoi@sit21c.com>
2024-07-18 20:35:09 +0900

    누락된 requestMapping 추가

--- src/main/java/com/sit21c/main/web/MainController.java
+++ src/main/java/com/sit21c/main/web/MainController.java
@@ -3,12 +3,15 @@ package com.sit21c.main.web;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+	@RequestMapping("/")
 	public String index(@RequestParam Map<String, Object> map) {
+		System.out.println("index 호출");
 		return "index";
 	}
 }
