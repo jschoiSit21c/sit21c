f2fd3096b8701f01a4d21e89c2f3492d29b8d493
juegy <juegy@DESKTOP-I6K6LR5.lan>
2024-11-20 23:27:07 +0900

    2024/11/20 자동 메일 발송 커밋

---
+++ src/main/java/com/sit21c/recruit/web/MailController.java
@@ -0,0 +1,38 @@
+package com.sit21c.recruit.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MailController {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @PostMapping("/submitApplication")
+    public String submitApplication(
+        @RequestParam("name") String name,
+        @RequestParam("email") String email,
+        @RequestParam("phone") String phone
+    ) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo("sitmento@gmail.com");
+            message.setSubject("[채용 지원] " + name + "님의 지원서");
+            message.setText("지원자 정보:\n\n"
+                    + "이름: " + name + "\n"
+                    + "이메일: " + email + "\n"
+                    + "전화번호: " + phone);
+
+            mailSender.send(message);
+            return "redirect:/recruit/recruitmentMain?success=true";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/recruit/recruitmentMain?error=true";
+        }
+    }
+}
